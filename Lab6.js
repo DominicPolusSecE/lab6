ea94e62661d738617e0f45ebb13a89915b32020d
DominicPolusSecE <n01708027@humber.ca>
2024-10-08 08:53:19 -0400

    Create Lab6.js

---
+++ Lab6.js
@@ -0,0 +1,28 @@
+let myNumber = 10;
+
+let myString = "hello dominic";
+
+console.log(myNumber);
+console.log(myString);
+
+for(let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
+for(let i = 1; i <= 5; i++) {
+    console.log(3 * i)
+}
+
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
+
+let sum = addNumbers(5,7);
+console.log(sum)
+
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+
+let product = multiplyNumbers(4,3);
+console.log(product);
